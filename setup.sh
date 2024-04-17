ac850e1a56eb9e97225b60d6c61a36ea2d1ae8df
holyhoppsan <daniel.hall@live.se>
2024-04-17 10:33:00 +0000

    Updated the setup script

--- setup.sh
+++ setup.sh
@@ -6,3 +6,19 @@ bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 
 ~/miniconda3/bin/conda init bash
+
+# Reload bash shell configuration to recognize Conda
+source ~/.bashrc
+
+# Create new Conda environment with Python 3.12
+~/miniconda3/bin/conda create --name week7day1 python=3.12 -y
+
+echo Attempting to print the environment
+# Activate the new environment
+source ~/miniconda3/bin/activate week7day1
+
+echo Environment set to:
+
+~/miniconda3/bin/conda info --envs
+#conda install cudatoolkit=11.0 -y
+pip install transformers datasets peft accelerate bitsandbytes
--- setupcondaenv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-conda create --name week7day1 python=3.12
-conda activate week7day1
